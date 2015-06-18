3128c1a3dd53eb57fca3a88d7096d4a58d83dae2
nowordforfree <nowordforfree@gmail.com>
2015-06-18 15:13:44 +0300

    working on animation function

--- slider/js/main.js
+++ slider/js/main.js
@@ -5,15 +5,18 @@ document.onreadystatechange = function () {
 }
 
 function Slider () {
-	var imgscount = 5;
+	this.images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+	this.current = 0;
 	this.body = document.createElement('div');
 	this.body.className = 'body';
-	this.body.style.width = (imgscount * 100) + '%';
-	for (var i = 0; i < imgscount; i++) {
+	this.body.style.width = (this.images.length * 100) + '%';
+	var self = this;
+
+	for (var i = 0; i < this.images.length; i++) {
 		var img = document.createElement('img');
-		img.src = 'img/' + (i + 1) + '.jpg';
-		img.style.width = (100 / imgscount) + '%';
-		img.style.left = i * (100 / imgscount) + '%';
+		img.src = 'img/' + this.images[i];
+		img.style.width = (100 / this.images.length) + '%';
+		img.style.left = i * (100 / this.images.length) + '%';
 		this.body.appendChild(img);
 	}
 
@@ -24,19 +27,61 @@ function Slider () {
 		// }
 	});
 
-	return this.body;
-}
-
-function move (elem, pix) {
-	setTimeout(function() {
-		var left = parseInt(elem.style.left) || 0;
-		if (pix > 0) {
-			elem.style.left = ++left + 'px';
-			pix--;
+	setInterval(function() {
+		if (self.current < self.images.length) {
+			animate(function (timePassed) {
+				self.body.style.left = -timePassed + 'px';
+			}, 1000);
+			self.current++;
 		} else {
-			elem.style.left = --left + 'px';
-			pix++;
+			animate(function (timePassed) {
+				self.body.style.left = '0px';
+			}, 1000);
+			self.current = 0;
 		}
-		if (pix) { move(elem, pix); };
-	}, 5);
+	}, 5000);
+
+	function animate(draw, duration) {
+		var start = performance.now();
+		requestAnimationFrame(function animate(time) {
+			// определить, сколько прошло времени с начала анимации
+			var timePassed = time - start;
+			// возможно небольшое превышение времени, в этом случае зафиксировать конец
+			if (timePassed > duration) { timePassed = duration; };
+			// нарисовать состояние анимации в момент timePassed
+			draw(timePassed);
+			// если время анимации не закончилось - запланировать ещё кадр
+			if (timePassed < duration) {
+				requestAnimationFrame(animate);
+			}
+		});
+	}
+
+	function delta(progress) {
+		return 1 - sin((1 - progress) * П/2);
+	}
+
+	// function draw (progress) {
+	// 	this.body.style.left = progress + 'px';
+	// }
+
+	// function move (elem, pix) {
+	// 	var timer = setInterval(function() {
+	// 		if (!pix) { clearInterval(timer); };
+	// 		var left = parseInt(elem.style.left) || 0;
+	// 		if (pix > 0) {
+	// 			elem.style.left = ++left + 'px';
+	// 			pix--;
+	// 		} else {
+	// 			elem.style.left = --left + 'px';
+	// 			pix++;
+	// 		}
+	// 	}, 5);
+	// }
+
+	// setInterval(function() {
+	// 	move(self.body, -850);
+	// }, 5000);
+	
+	return this.body;
 }
